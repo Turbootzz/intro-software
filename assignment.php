5cd366a00267aa4f3f330092fffe08c71ba29d6c
Thijs Herman <tc.herman@hotmail.com>
2022-11-15 11:15:09 +0100

    font and begin of ace

--- assignment.php
+++ assignment.php
@@ -3,13 +3,7 @@
 ?>
 <section class="main-container">
   <div class="container">
-<pre><code class="javascript">
-$(document).ready(function() {
-  $('pre code').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
-});
-</code></pre>
+  
 </div>
 </section>
 <?php 
--- footer.php
+++ footer.php
@@ -1,18 +1,116 @@
 </main>
 <footer>
-
-      <ul>
-            <li>
-                  <img src="assets/img/icon-white.png" alt="Logo">
-            </li>
-      </ul>
-
+      <section>
+            <div class=footer-container>
+                  <ul class="footer-grid-container">
+                        <li class="footer-logo-container">
+                              <a class="footer-logo" href="index.php"><img src="assets/img/icon-white.png" alt="Logo"></a>
+                        </li>
+                        <li class="footer-section-1">
+                              <h2 class="footer-link-header">Informatie</h2>
+                              <ul class=footer-links>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Over ons</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Werken bij</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Jaarkalender</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Downloads</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Artikelen</a>
+                                    </li>
+                              </ul>
+                        </li>
+                        <li class="footer-section-2">
+                              <h2 class="footer-link-header">Studenten</h2>
+                              <ul class=footer-links>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Stages</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Talentprogramma</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Inloggen ELO</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Online Expositie</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">GLU Alumni</a>
+                                    </li>
+                              </ul>
+                        </li>
+                        <li class="footer-section-3">
+                              <h2 class="footer-link-header">Opleidingen</h2>
+                              <ul class=footer-links>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Alle opleidingen</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Aanmelden</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Keuzedelen</a>
+                                    </li>
+                              </ul>
+                        </li>
+                        <li class="footer-section-4">
+                              <h2 class="footer-link-header">Bedrijven</h2>
+                              <ul class=footer-links>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Stagebedrijven</a>
+                                    </li>
+                                    <li>
+                                          <a href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/">Het Bureau</a>
+                                    </li>
+                              </ul>
+                        </li>
+                        <li class="footer-section-5">
+                              <ul>
+                                    <li>
+                                          <a href="#">Disclaimer</a>
+                                    </li>
+                                    <li>
+                                          <a href="#">Privacy</a>
+                                    </li>
+                                    <li>
+                                          <a href="#">Colofon</a>
+                                    </li>
+                                    <li>
+                                          <a href="#">Klachten</a>
+                                    </li>
+                                    <li>
+                                          <a href="#">Contact</a>
+                                    </li>
+                              </ul>
+                        </li>
+                        <li class="footer-social-media">
+                              <ul>
+                                    <li>
+                                          <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+                                    </li>
+                                    <li>
+                                          <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                    </li>
+                                    <li>
+                                          <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                                    </li>
+                              </ul>
+                        </li>
+                  </ul>
+            </div>
+      </section>
 </footer>
 <!-- javascript files -->
 <script type="text/javascript" src="assets/js/main.js"></script>
 <link rel="stylesheet"
       href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/default.min.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></script>
-<script>hljs.initHighlightingOnLoad();</script>
-</body>
-</html>
+<script src="https://kit.fontawesome.com/dc7f6e94f0.js" crossorigin="anonymous"></script>
+<scr
