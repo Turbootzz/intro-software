030536d10615c88dc859707cc5ee07b9e13d0be3
Thijs Herman <tc.herman@hotmail.com>
2022-12-13 10:19:29 +0100

    front-end changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,8 @@
-console.log("testt");
+console.log("GLU");
 
 const html_code = document.querySelector('.html-code textarea');
 const css_code = document.querySelector('.css-code textarea');
-const 
-js_code = document.querySelector('.js-code textarea');
-const cs_code = document.querySelector('.cs-code textarea');
+const js_code = document.querySelector('.js-code textarea');
 const result = document.querySelector('#result');
 
 function run() {
